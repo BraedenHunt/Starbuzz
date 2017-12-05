634bc3e7981f3abbf5c259158486d0a0d851c5ed
bjhunt7520 <20hunbra@student.mcpsmt.org>
2017-12-05 10:34:50 -0700

    Started Starbuzz

--- app/src/main/java/org/pltw/examples/starbuzz/TopLevelActivity.java
+++ app/src/main/java/org/pltw/examples/starbuzz/TopLevelActivity.java
@@ -2,6 +2,8 @@ package org.pltw.examples.starbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                  
+            }
+        };
     }
 }
